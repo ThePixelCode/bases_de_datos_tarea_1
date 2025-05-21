728d2766971538b1f8457424aae7ba17ddf542b3
ThePixelCode <thepixelcode@proton.me>
2025-05-20 23:16:17 -0500

    Updated main page

--- src/react/MainPage.jsx
+++ src/react/MainPage.jsx
@@ -1,3 +1,27 @@
+import { Toaster } from "@/components/ui/sonner";
+import Dialogs from "./Dialogs";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
 export default function MainPage() {
-  return (<h1>Hello</h1>);
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+          },
+        },
+      }),
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Dialogs />
+      <Toaster />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
