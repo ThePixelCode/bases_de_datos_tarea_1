d17c863574fce36a4b9bf83d6a3f575190916e4b
ThePixelCode <thepixelcode@proton.me>
2025-05-20 23:07:11 -0500

    Updated schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,22 @@
 import { relations, sql } from "drizzle-orm";
-import { boolean, check, integer, interval, jsonb, pgTable, primaryKey, text, varchar } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  check,
+  integer,
+  interval,
+  jsonb,
+  pgTable,
+  primaryKey,
+  text,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const developerTable = pgTable("DEVELOPERS_PUBLISHERS", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).unique().notNull(),
 });
 
-export const developerRelations = relations(developerTable, ({many}) => ({
+export const developerRelations = relations(developerTable, ({ many }) => ({
   games: many(gameTable),
 }));
 
@@ -14,11 +24,21 @@ export const gameTable = pgTable("GAMES", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).unique().notNull(),
   price: integer().notNull(),
-  developer: integer().notNull().references(()=>developerTable.id, {onDelete: "cascade", onUpdate: "cascade"}),
-  publisher: integer().notNull().references(()=>developerTable.id, {onDelete: "cascade", onUpdate: "cascade"}),
+  developer: integer()
+    .notNull()
+    .references(() => developerTable.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+  publisher: integer()
+    .notNull()
+    .references(() => developerTable.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
 });
 
-export const gameRelations = relations(gameTable, ({one, many}) => ({
+export const gameRelations = relations(gameTable, ({ one, many }) => ({
   publisher: one(developerTable, {
     fields: [gameTable.publisher],
     references: [developerTable.id],
@@ -35,18 +55,30 @@ export const tagTable = pgTable("TAGS", {
   name: varchar({ length: 255 }).unique().notNull(),
 });
 
-export const tagRelations = relations(tagTable, ({many}) => ({
+export const tagRelations = relations(tagTable, ({ many }) => ({
   gameToTags: many(gameToTagsTable),
 }));
 
-export const gameToTagsTable = pgTable("GAMETOTAGS", {
-  gameId: integer("game_id").notNull().references(()=>gameTable.id, {onDelete: "cascade", onUpdate: "cascade"}),
-  tagId: integer("tag_id").notNull().references(()=>tagTable.id, {onDelete: "cascade", onUpdate: "cascade"}),
-}, (table) => [
-  primaryKey({ columns: [table.gameId, table.tagId] }),
-]);
+export const gameToTagsTable = pgTable(
+  "GAMETOTAGS",
+  {
+    gameId: integer("game_id")
+      .notNull()
+      .references(() => gameTable.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      }),
+    tagId: integer("tag_id")
+      .notNull()
+      .references(() => tagTable.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      }),
+  },
+  (table) => [primaryKey({ columns: [table.gameId, table.tagId] })],
+);
 
-export const gameToTagsRelations = relations(gameToTagsTable, ({one}) => ({
+export const gameToTagsRelations = relations(gameToTagsTable, ({ one }) => ({
   game: one(gameTable, {
     fields: [gameToTagsTable.gameId],
     references: [gameTable.id],
@@ -62,61 +94,104 @@ export const playerTable = pgTable("PLAYERS", {
   username: varchar({ length: 255 }).unique().notNull(),
 });
 
-export const playerRelations = relations(playerTable, ({many})=>({
+export const playerRelations = relations(playerTable, ({ many }) => ({
   playerToGame: many(playerToGameTable),
   user1: many(friendshipTable),
   user2: many(friendshipTable),
 }));
 
-export const playerToGameTable = pgTable("PLAYERTOGAMES", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  gameId: integer("game_id").notNull().references(()=>gameTable.id, {onDelete: "cascade", onUpdate: "cascade"}),
-  playerId: integer("player_id").notNull().references(()=>playerTable.id, {onDelete: "cascade", onUpdate: "cascade"}),
-  playtime: interval().notNull().default("0 seconds"),
-  recommended: boolean(),
-  review: text(),
-}, (table) => [
-  check("review", sql`(${table.review} IS NULL AND ${table.recommended} IS NULL) OR (${table.review} IS NOT NULL AND ${table.recommended} IS NOT NULL)`),
-]);
+export const playerToGameTable = pgTable(
+  "PLAYERTOGAMES",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    gameId: integer("game_id")
+      .notNull()
+      .references(() => gameTable.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      }),
+    playerId: integer("player_id")
+      .notNull()
+      .references(() => playerTable.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      }),
+    playtime: interval().notNull().default("0 seconds"),
+    recommended: boolean(),
+    review: text(),
+  },
+  (table) => [
+    check(
+      "review",
+      sql`(${table.review} IS NULL AND ${table.recommended} IS NULL) OR (${table.review} IS NOT NULL AND ${table.recommended} IS NOT NULL)`,
+    ),
+  ],
+);
 
-export const playerToGameRelations = relations(playerToGameTable, ({one, many})=>({
-  savePlays: many(savedPlayTable),
-  player: one(playerTable, {
-    fields: [playerToGameTable.playerId],
-    references: [playerTable.id],
+export const playerToGameRelations = relations(
+  playerToGameTable,
+  ({ one, many }) => ({
+    savePlays: many(savedPlayTable),
+    player: one(playerTable, {
+      fields: [playerToGameTable.playerId],
+      references: [playerTable.id],
+    }),
+    game: one(gameTable, {
+      fields: [playerToGameTable.gameId],
+      references: [gameTable.id],
+    }),
   }),
-  game: one(gameTable, {
-    fields: [playerToGameTable.gameId],
-    references: [gameTable.id],
-  }),
-}))
+);
 
 export const savedPlayTable = pgTable("SAVEDPLAYS", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  gameToPlayerId: integer("game_to_player_id").notNull().references(()=>playerToGameTable.id, {onDelete: "cascade", onUpdate: "cascade"}),
+  gameToPlayerId: integer("game_to_player_id")
+    .notNull()
+    .references(() => playerToGameTable.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
   file: varchar({ length: 255 }).notNull(),
   link: varchar({ length: 255 }).notNull(),
 });
 
-export const savedPlayRelations = relations(savedPlayTable, ({one})=>({
+export const savedPlayRelations = relations(savedPlayTable, ({ one }) => ({
   gamePlayer: one(playerToGameTable, {
     fields: [savedPlayTable.gameToPlayerId],
     references: [playerToGameTable.id],
   }),
 }));
 
-export const friendshipTable = pgTable("FRIENDSHIP", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  user1: integer("user1_id").notNull().references(()=>playerTable.id, {onDelete: "cascade", onUpdate: "cascade"}),
-  user2: integer("user2_id").notNull().references(()=>playerTable.id, {onDelete: "cascade", onUpdate: "cascade"}),
-  are_friends: boolean().notNull(),
-  messages: jsonb().$type<{user: number, message: string}[]>().notNull(),
-},(table)=>[
-  check("messages", sql`validate_messages(${table.messages}, (ARRAY[${table.user1}::TEXT, ${table.user2}::TEXT])::TEXT[])`),
-  check("users", sql`${table.user1} < ${table.user2}`),
-]);
+// This is not actually a friendship is more like an acknowledge
+export const friendshipTable = pgTable(
+  "FRIENDSHIP",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    user1: integer("user1_id")
+      .notNull()
+      .references(() => playerTable.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      }),
+    user2: integer("user2_id")
+      .notNull()
+      .references(() => playerTable.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      }),
+    are_friends: boolean().notNull(),
+    messages: jsonb().$type<{ user: number; message: string }[]>().notNull(),
+  },
+  (table) => [
+    check(
+      "messages",
+      sql`validate_messages(${table.messages}, (ARRAY[${table.user1}::TEXT, ${table.user2}::TEXT])::TEXT[])`,
+    ),
+    check("users", sql`${table.user1} < ${table.user2}`),
+  ],
+);
 
-export const friendshipRelations = relations(friendshipTable, ({one})=>({
+export const friendshipRelations = relations(friendshipTable, ({ one }) => ({
   user: one(playerTable, {
     fields: [friendshipTable.user1, friendshipTable.user2],
     references: [playerTable.id, playerTable.id],
