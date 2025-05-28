8070d3658452d04afda719c4ce72c2f6b81fa253
ThePixelCode <thepixelcode@proton.me>
2025-05-28 00:44:43 -0500

    Refactor Result

--- src/lib/result.ts
+++ src/lib/result.ts
@@ -1,96 +1,137 @@
-type Err<E> = { ok: null; err: E };
-type Ok<T> = { ok: T; err: null };
-type Res<T, E> = Ok<T> | Err<E>;
+const ERR = Symbol("Result.Err");
+const OK = Symbol("Result.Ok");
 export class Result<T, E> {
-  constructor(private inner: Res<T, E>) {}
+  private constructor(
+    private ok: T | typeof OK,
+    private err: E | typeof ERR,
+  ) {}
 
   public map<U>(callback: (ok: T) => U): Result<U, E> {
-    const inner = this.inner;
-    if (inner.err) {
-      return new Result({ ok: null, err: inner.err }) as unknown as Result<
-        U,
-        E
-      >;
+    if (this.is_err()) {
+      return this as Result<unknown, E> as Result<U, E>;
     }
-    return new Result({
-      ok: callback(inner.ok as T),
-      err: null,
-    }) as unknown as Result<U, E>;
+    return Result.ok(callback(this.ok as T));
+  }
+
+  public map_err<U>(callback: (err: E) => U): Result<T, U> {
+    if (this.is_ok()) {
+      return this as Result<T, unknown> as Result<T, U>;
+    }
+    return Result.err(callback(this.err as E));
   }
 
   public is_ok(): boolean {
-    if (this.inner.ok !== null) {
+    if (this.ok !== OK) {
       return true;
     }
     return false;
   }
 
   public is_err(): boolean {
-    if (this.inner.err !== null) {
+    if (this.err !== ERR) {
       return true;
     }
     return false;
+  }
+
+  public getInner(): { ok: T; err: null } | { ok: null; err: E } {
+    if (this.is_err()) {
+      return { ok: null, err: this.err as E };
+    }
+    return { ok: this.ok as T, err: null };
   }
 
   public return_err<U>(): Result<U, E> {
     if (this.is_ok()) {
       throw new Error("Got Ok when expected Err");
     }
-    return this as unknown as Result<U, E>;
-  }
-
-  public getInner(): Res<T, E> {
-    return this.inner;
+    return this as Result<unknown, E> as Result<U, E>;
   }
 
   public unwrap(): T {
-    if (this.inner.ok === null) {
+    if (this.is_err()) {
       throw new Error("tried to unwrap on Err!");
     }
-    return this.inner.ok;
+    return this.ok as T;
   }
 
   public unwrap_err(): E {
-    if (this.inner.err === null) {
+    if (this.is_ok()) {
       throw new Error("tried to unwrap_err on Ok!");
     }
-    return this.inner.err;
+    return this.err as E;
   }
 
-  public map_err<U>(callback: (err: E) => U): Result<T, U> {
-    const inner = this.inner;
-    if (inner.ok) {
-      return new Result({ err: null, ok: inner.ok }) as unknown as Result<T, U>;
+  public static try<T>(callback: () => T): Result<T, Error> {
+    try {
+      return Result.ok(callback());
+    } catch (err) {
+      if (err instanceof Error) {
+        return Result.err(err);
+      }
+      return Result.err(new Error(`Unknown error: ${err}`));
     }
-    return new Result({
-      err: callback(inner.err as E),
-      ok: null,
-    }) as unknown as Result<T, U>;
+  }
+
+  public static tryArgs<T, F>(
+    callback: (args: F) => T,
+    args: F,
+  ): Result<T, Error> {
+    try {
+      return Result.ok(callback(args));
+    } catch (err) {
+      if (err instanceof Error) {
+        return Result.err(err);
+      }
+      return Result.err(new Error(`Unknown error: ${err}`));
+    }
+  }
+
+  public static async tryAsync<T>(
+    callback: () => Promise<T>,
+  ): Promise<Result<T, Error>> {
+    try {
+      return Result.ok(await callback());
+    } catch (err) {
+      if (err instanceof Error) {
+        return Result.err(err);
+      }
+      return Result.err(new Error(`Unknown error: ${err}`));
+    }
+  }
+
+  public static async tryAsyncArgs<T, F>(
+    callback: (args: F) => Promise<T>,
+    args: F,
+  ): Promise<Result<T, Error>> {
+    try {
+      return Result.ok(await callback(args));
+    } catch (err) {
+      if (err instanceof Error) {
+        return Result.err(err);
+      }
+      return Result.err(new Error(`Unknown error: ${err}`));
+    }
   }
 
   static collect<T, E>(arr: Result<T, E>[]): Result<T[], E> {
     const arrT: T[] = [];
     for (let i = 0; i < arr.length; i++) {
       let res = arr[i];
-      let inner = res.inner;
-      if (inner.err) {
-        // This is perfectly fine but the compiler doesn't shut up about it
-        return new Result({ ok: null, err: inner.err }) as unknown as Result<
-          T[],
-          E
-        >;
+      if (res.ok !== OK) {
+        arrT[i] = res.ok;
       } else {
-        arrT[i] = inner.ok as T;
+        return Result.err(res.err as E);
       }
     }
-    return new Result({ ok: arrT, err: null }) as unknown as Result<T[], E>;
+    return Result.ok(arrT);
   }
 
   static ok<T, E>(ok: T): Result<T, E> {
-    return new Result({ ok: ok, err: null }) as unknown as Result<T, E>;
+    return new Result(ok, ERR) as Result<T, E>;
   }
 
   static err<T, E>(err: E): Result<T, E> {
-    return new Result({ ok: null, err: err }) as unknown as Result<T, E>;
+    return new Result(OK, err) as Result<T, E>;
   }
 }
